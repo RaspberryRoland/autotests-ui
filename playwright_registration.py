e46fa05ad92db60900ccf99a9208ad3f8a6d1287
RaspberryRoland <valiev.rob@mail.ru>
2026-05-14 08:04:16 +0500

    Работа с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -3,7 +3,9 @@ from playwright.sync_api import sync_playwright, expect
 
 with sync_playwright() as playwright:
     browser = playwright.firefox.launch(headless=False)
-    page = browser.new_page()
+    #page = browser.new_page()
+    context = browser.new_context()
+    page = context.new_page()
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
@@ -20,6 +22,17 @@ with sync_playwright() as playwright:
     registration_button = page.get_by_test_id('registration-page-registration-button')
     registration_button.click()
 
+    context.storage_state(path='browser-state.json')
+
     dashboard_header = page.get_by_test_id('dashboard-toolbar-title-text')
     expect(dashboard_header).to_be_visible()
     expect(dashboard_header).to_have_text('Dashboard')
+
+with sync_playwright() as playwright:
+    browser = playwright.firefox.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+
+    page.wait_for_timeout(5000)

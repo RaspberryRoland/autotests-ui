557ef6023c0ba126838c01501f9d5afbd6cd3f6b
RaspberryRoland <valievrob@mail.ru>
2026-06-04 23:53:20 +0500

    Наращивание тестовой базы и рефакторинг

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -3,8 +3,10 @@ from typing import Any, Generator
 import pytest
 from playwright.sync_api import Playwright, Page
 
+from pages.authentication.registration_page import RegistrationPage
 
-@pytest.fixture(scope="function")
+
+@pytest.fixture
 def chromium_page(playwright: Playwright) -> Generator[Page, Any, None]:
     browser = playwright.chromium.launch(headless=False)
     yield browser.new_page()
@@ -17,27 +19,17 @@ def initialize_browser_state(playwright: Playwright):
     context = browser.new_context()
     page = context.new_page()
 
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-
-    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill('user.name@gmail.com')
-
-    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.fill('username')
-
-    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill('password')
-
-    registration_button = page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
+    registration_page = RegistrationPage(page=page)
+    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.registration_form_component.fill(email='user.name@gmail.com', username='username', password='password')
+    registration_page.click_registration_button()
 
     context.storage_state(path="browser-state.json")
     browser.close()
 
 
-@pytest.fixture(scope="function")
-def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> \
-Generator[Page, Any, None]:
+@pytest.fixture
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Generator[Page, Any, None]:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state="browser-state.json")
     yield context.new_page()

99642f3405a838a9270147dbd7575f5bb28995b2
RaspberryRoland <valiev.rob@mail.ru>
2026-06-10 08:08:31 +0500

    Добавление видео в Allure отчет

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,17 +1,16 @@
 from typing import Any, Generator
 
 import pytest
+from _pytest.fixtures import SubRequest
 from playwright.sync_api import Playwright, Page
 
 from pages.authentication.registration_page import RegistrationPage
+from tools.playwright.pages import initialize_playwright_page
 
 
 @pytest.fixture
-def chromium_page(playwright: Playwright) -> Generator[Page, Any, None]:
-    browser = playwright.chromium.launch(headless=False)
-    yield browser.new_page()
-    browser.close()
-
+def chromium_page(request: SubRequest, playwright: Playwright) -> Generator[Page, Any, None]:
+    yield from initialize_playwright_page(playwright, test_name=request.node.name)
 
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
@@ -29,8 +28,8 @@ def initialize_browser_state(playwright: Playwright):
 
 
 @pytest.fixture
-def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Generator[Page, Any, None]:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state="browser-state.json")
-    yield context.new_page()
-    browser.close()
+def chromium_page_with_state(request: SubRequest,initialize_browser_state, playwright: Playwright) -> Generator[Page, Any, None]:
+    yield from initialize_playwright_page(
+        playwright,
+        test_name=request.node.name,
+        storage_state="browser-state.json")

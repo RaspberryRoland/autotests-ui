988efa0d0ca016483a523a752d5decef30c3cb4e
RaspberryRoland <valiev.rob@mail.ru>
2026-06-17 08:35:53 +0500

    Добавляем информацию о переменных окружения в Allure отчет. Практика работы с переменными окружения в Allure

--- tools/allure/environment.py
+++ tools/allure/environment.py
@@ -1,9 +1,15 @@
+import platform
+import sys
+
 from config import settings
 
 
 def create_allure_environment_file():
-    items = [f'{key} = {value}' for key, value in settings.model_dump().items()]
-    properties = '\n'.join(items)
+    items = [f'{key} = {value}' for key, value in
+             settings.model_dump().items()]
+    properties = '\n'.join(
+        items) + f'\nos_info={platform.system()}, {platform.release()}' + f'\npython_version={sys.version}'
 
-    with open(settings.allure_results_dir.joinpath('environment.properties'), 'w+') as file:
+    with open(settings.allure_results_dir.joinpath('environment.properties'),
+              'w+') as file:
         file.write(properties)
